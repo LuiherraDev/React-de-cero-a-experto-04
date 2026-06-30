d0eb4a8c83d27d05506b6f28359ab2eff0b309c6
luis <luiherra25@gmail.com>
2026-06-30 17:25:40 +0200

    Starting with useRef

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import { createRoot } from 'react-dom/client'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
-import { PokemonPage } from './03-examples/PokemonPage'
+// import { PokemonPage } from './03-examples/PokemonPage'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 import './index.css'
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
 )
