851fdeea9b9f34a4c6f7a524e3905444ff524c82
luis <luiherra25@gmail.com>
2026-07-01 15:32:04 +0200

    Starting WordsApp with useReducer

---
+++ src/05-useReducer/reducer/scrambleWordReducer.ts
@@ -0,0 +1,86 @@
+export interface ScrambleWordState {
+  currentWord: string;
+  errorCounter: number;
+  guess: string;
+  isGameOver: boolean;
+  maxAllowErrors: number;
+  maxSkips: number;
+  points: number;
+  scrambledWord: string;
+  skipCounter: number;
+  words: string[];
+  totalWords: number;
+
+}
+
+const GAME_WORDS = [
+  'REACT',
+  'JAVASCRIPT',
+  'TYPESCRIPT',
+  'HTML',
+  'ANGULAR',
+  'SOLID',
+  'NODE',
+  'VUEJS',
+  'SVELTE',
+  'EXPRESS',
+  'MONGODB',
+  'POSTGRES',
+  'DOCKER',
+  'KUBERNETES',
+  'WEBPACK',
+  'VITE',
+  'TAILWIND',
+];
+
+// Esta función mezcla el arreglo para que siempre sea aleatorio
+const shuffleArray = (array: string[]) => {
+  return array.sort(() => Math.random() - 0.5);
+};
+
+// Esta función mezcla las letras de la palabra
+const scrambleWord = (word: string = '') => {
+  return word
+    .split('')
+    .sort(() => Math.random() - 0.5)
+    .join('');
+};
+
+export const getInicialState = (): ScrambleWordState => {
+  const shuffledWords = shuffleArray([...GAME_WORDS])
+
+
+  return {
+
+    currentWord: shuffledWords[0],
+    errorCounter: 0,
+    guess: '',
+    isGameOver: false,
+    maxAllowErrors: 3,
+    maxSkips: 3,
+    points: 0,
+    scrambledWord: scrambleWord(shuffledWords[0]),
+    skipCounter: 0,
+    words: shuffledWords,
+    totalWords: shuffledWords.length,
+  }
+}
+
+export type ScrambleWordAction =
+  | { type: 'NO_SE_AUN' }
+  | { type: 'NO_SE_AUN' }
+  | { type: 'NO_SE_AUN' }
+
+export const scrambleWordReducer = (state: ScrambleWordState, action: ScrambleWordAction) => {
+
+  switch (action.type) {
+
+
+    default:
+      return state
+  }
+
+}
+
+
+
